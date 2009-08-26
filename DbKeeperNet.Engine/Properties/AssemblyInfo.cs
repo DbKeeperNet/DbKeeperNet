d9d26c3a482bdd93937b529d5a08ebaa4ef5f5a4
Vladimir Kloz <Vladimir.kloz@gmail.com>
2009-08-26 21:08:14 +0000

    Strong name key

--- DbKeeperNet.Engine/Properties/AssemblyInfo.cs
+++ DbKeeperNet.Engine/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -33,3 +34,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.1.1")]
 [assembly: AssemblyFileVersion("1.0.1.1")]
+
+// [assembly: CLSCompliant(true)]
