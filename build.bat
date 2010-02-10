5efc8d19252d511d72669c86885ad06d1e705b9d
Vladimir Kloz <Vladimir.kloz@gmail.com>
2010-02-10 20:45:00 +0000

    New build process using MSBuild

---
+++ build.bat
@@ -0,0 +1,7 @@
+if "%1"  "" goto startBuild
+echo Enter a target from build script (SrcZip,Build,BinZip)
+
+:startBuild
+C:\WINDOWS\Microsoft.NET\Framework\v3.5\msbuild.exe DbKeeperNet.xml /Target:%1 /l:FileLogger,Microsoft.Build.Engine;logfile=DbKeeperNetBuild.log
+
+:end
