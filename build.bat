5ecd9229268bce4d80cc3a3236b29eadbfecde51
voloda <vladimir.kloz@gmail.com>
2014-07-26 21:21:33 +0200

    Issue #5: Create nuget packages as a part of msbuild script

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@ rem if "%1"  "" goto startBuild
 echo Enter a target from build script (SrcZip,Build,BinZip)
 
 :startBuild
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe DbKeeperNet.xml /Target:%1 /l:FileLogger,Microsoft.Build.Engine;logfile=DbKeeperNetBuild.log
+C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe DbKeeperNet.msbuild /Target:%1 /l:FileLogger,Microsoft.Build.Engine;logfile=DbKeeperNetBuild.log
 
 :end
