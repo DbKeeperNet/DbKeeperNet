20d298f78ce6e4ebe2ba666379468b67d3ad5f14
voloda <vladimir.kloz@gmail.com>
2014-11-21 19:54:37 +0100

    Issue #24: Reorganize demo projects & add demo project for AspNetMembership features

---
+++ Demos/DbKeeperNet.SimpleDemo/Program.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DbKeeperNet.Engine;
+using System.Data.Common;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace DbKeeperNet.SimpleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // const string connString = "pgsql"; // PostgreSql connection over .NET Connector
+            // const string connString = "mysql"; // MySql connection over .NET Connector
+            // const string connString = "sqlite"; // SQLite connection over .NET Connector
+            const string connString = "default"; // MsSql connection
+            // const string connString = "oracle"; // MsSql connection
+
+            using (UpdateContext context = new UpdateContext())
+            {
+                context.LoadExtensions();
+                context.InitializeDatabaseService(connString);
+
+                Updater updater = new Updater(context);
+                updater.ExecuteXmlFromConfig();
+            }
+
+            ConnectionStringSettings connectString = ConfigurationManager.ConnectionStrings[connString];
+
+            using (DbConnection connection = DbProviderFactories.GetFactory(connectString.ProviderName).CreateConnection())
+            {
+                connection.ConnectionString = connectString.ConnectionString;
+                connection.Open();
+
+                DbCommand cmd = connection.CreateCommand();
+                cmd.CommandText = "select * from DbKeeperNet_SimpleDemo";
+                DbDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                    Console.WriteLine("{0}: {1}", reader[0], reader[1]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
